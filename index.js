3a54c6f4cab0c0557c4c787c6dfa25daf06cdb3a
Matthew Delisle <matthewdelisle@MatthewislesMBP.home>
2017-12-06 14:51:26 -0500

    trafficLights

---
+++ index.js
@@ -0,0 +1,46 @@
+function doTrafficLights() {
+  var activeLight = getActiveLight();
+   if (activeLight === 'red') {
+    turnRed();
+  }
+  if (activeLight === 'yellow') {
+    turnYellow();
+  }
+  if (activeLight === 'green') {
+    turnGreen();
+  }
+}
+  let activeLight=doTrafficLights('red, yellow, green');
+  console.log(activeLight);
+
+
+function turnOffLights() {
+  $('.traffic-light').removeClass('yellow-on red-on green-on');
+}
+
+function turnGreen() {
+  turnOffLights();
+  $('.green-light').addClass('green-on');
+}
+
+function turnYellow() {
+  turnOffLights();
+  $('.yellow-light').addClass('yellow-on');
+}
+
+function turnRed() {
+  turnOffLights();
+  $('.red-light').addClass('red-on');
+}
+
+function getActiveLight() {
+  return (['red', 'green', 'yellow'])[Math.floor(Math.random() * 3)];
+}
+
+function handleClicks() {
+  $('.js-control-lights').click(function() {
+    doTrafficLights();
+  });
+}
+
+$(handleClicks);
